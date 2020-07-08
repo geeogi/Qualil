43176bf569bda3f3ac2f7cb5746f067512c5d6ab
George Bennett <george6bennett@gmail.com>
2020-07-08 21:23:57 +0100

    abbreviate large numbers

--- src/Core/numberUtils.ts
+++ src/Core/numberUtils.ts
@@ -21,14 +21,47 @@ export const getScaleMethod = (
   };
 };
 
-export const numberToString = (x: number) => {
-  return x.toLocaleString();
-};
-
+/**
+ * 0.433576894 => `0.433`
+ * @param x
+ */
 export const numberWithSignificantDigits = (x: number) => {
   return parseFloat(x.toPrecision(4)).toLocaleString();
 };
 
+/**
+ * Rounds to nearest multiple
+ * @param num
+ * @param multiple
+ */
 export const roundToNearest = (num: number, multiple: number) => {
   return Math.round(num / multiple) * multiple;
+};
+
+/**
+ * 8962055 => `8.9M`
+ * 8962053455 => `8.9B`
+ * @param num
+ */
+export const abbreviate = (num: number) => {
+  if (num > 1000000000) {
+    const rounded = parseFloat(num.toPrecision(2));
+    return `${rounded / 1000000000}B`;
+  } else if (num > 1000000) {
+    const rounded = parseFloat(num.toPrecision(2));
+    return `${rounded / 1000000}M`;
+  } else {
+    return num;
+  }
+};
+
+/**
+ * @param x
+ */
+export const numberToString = (x: number) => {
+  if (x > 1000000) {
+    return abbreviate(x);
+  } else {
+    return x.toLocaleString();
+  }
 };
