0553e07b2b4829c9d879ed8b790d8eaa5d304875
George Bennett <george6bennett@gmail.com>
2020-07-09 13:57:36 +0100

    use hex code for transparency in Label

--- src/Components/Graph/Label.tsx
+++ src/Components/Graph/Label.tsx
@@ -1,13 +1,18 @@
 import React from "react";
+import { BACKGROUND_COLOR } from "../../Config/colors";
+
+const TRANSPARENCY_HEX = "99";
+
 export const Label = (props: { text: string; top: number; left: number }) => (
   <label
     style={{
       position: "absolute",
       top: props.top,
       left: props.left,
-      background: "rgba(0,0,0,0.3)",
-      padding: "1px",
+      background: `${BACKGROUND_COLOR}${TRANSPARENCY_HEX}`,
+      padding: "1px 3px",
       marginTop: "1px",
+      borderRadius: "0 3px 3px 0",
     }}
   >
     {props.text}
