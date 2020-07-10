a659cc7437bbf8f913db9eee263146ccfc26afca
George Bennett <george6bennett@gmail.com>
2020-07-10 22:11:11 +0100

    disable select of Asset attributes

--- src/Components/Asset/Attribute.tsx
+++ src/Components/Asset/Attribute.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { ATTRIBUTE_COLOUR } from "../../Config/colors";
 
 export const Attribute = (props: { attrib: string; value: string }) => (
-  <span className="small-font">
+  <span className="small-font non-select">
     <span style={{ color: ATTRIBUTE_COLOUR }}>{props.attrib}</span>{" "}
     {props.value}
   </span>
