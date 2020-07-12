33f6ccf856f606eddaa472827bad549e0fb485b6
George Bennett <george6bennett@gmail.com>
2020-07-12 20:36:54 +0100

    improve subtitle position on mobile

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -7,7 +7,7 @@ export const Header = () => (
       style={{ alignItems: "flex-end", justifyContent: "space-between" }}
     >
       <h1 className="ma0 Title">Qualil</h1>
-      <p className="ma0 attribute" style={{ marginLeft: "8px" }}>
+      <p className="ma0 mt8 attribute">
         Cryptocurrency analytics powered by CoinGecko.
       </p>
     </div>
